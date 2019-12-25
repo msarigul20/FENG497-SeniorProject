6ca4fb0a658cd9dcb6c098b3c42129854ab2c1de
msarigul20 <m.sarigul199@gmail.com>
2019-12-25 18:12:45 +0300

    added  two method for two sensor

--- Python Codes/socket1.py
+++ Python Codes/socket1.py
@@ -1,36 +1,3 @@
-#import socket
-
-#socket = socket.socket()
-
-#host = "192.168.2.4"  # ESP IP in local network
-#port = 6896  # ESP32 Server Port
-
-#socket.connect((host, port))
-#message = "H"
-#socket.send(message)
-
-#data = ""
-
-#while len(data) < len(message):
-    #data += socket.recv(1)
-
-#print(data)
-
-#socket.close()
-"""
-import socket
-
-HOST = '192.168.2.4'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'L')
-    data = s.recv(1024)
-
-print('Received', repr(data))
-"""
-
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,22 +6,22 @@ port =6896
 s.connect((host,port))
 
 def ts(str):
-   s.send('e'.encode())
-
-
+   s.send('w'.encode())
    data = ''
    data = s.recv(2048).decode()
+   print("Water Sensor Value : ","<",data,">")
+#data=''
+def tss(str):
+   s.send('f'.encode())
+   data1 = ''
+   data1 = s.recv(2048).decode()
+   print("Flame Sensor Value : ","<",data1,">")
+   #1 its okey there is not flame in environment
 
-
-
-   print(data[0:]+"\n")
-
-data=''
-
-
+#data = ''
 while 2:
    r = input('enter')
    ts(s)
+   tss(s)
 s.close ()
 
-#hi world to try
