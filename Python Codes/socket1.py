86958d6fee998a166c6fb75437d792373b77ceb2
Mustafa Sarigul <m.sarigul199@gmail.com>
2019-12-25 18:02:02 +0300

    mistakes fix about commit

--- Python Codes/socket1.py
+++ Python Codes/socket1.py
@@ -1,36 +1,3 @@
-#import socket
-
-#socket = socket.socket()
-
-#host = "192.168.2.4"  # ESP IP in local network
-#port = 6896  # ESP32 Server Port
-
-#socket.connect((host, port))
-#message = "H"
-#socket.send(message)
-
-#data = ""
-
-#while len(data) < len(message):
-    #data += socket.recv(1)
-
-#print(data)
-
-#socket.close()
-"""
-import socket
-
-HOST = '192.168.2.4'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'L')
-    data = s.recv(1024)
-
-print('Received', repr(data))
-"""
-
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
