6dace02d00b0f31a67955f5664fc5ae6699d38d5
Mustafa Sarigul <m.sarigul199@gmail.com>
2019-12-28 16:20:29 +0300

    fixing commits

--- Python Codes/socket1.py
+++ Python Codes/socket1.py
@@ -1,10 +1,17 @@
+#code works 2 different board.
 import socket
-
+#first socket created
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host ="192.168.2.4"
 port =6896
 s.connect((host,port))
 
+#second socket created
+s2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host2 ="192.168.2.5"
+port2 =6897
+s2.connect((host2,port2))
+#functions for measuring the sensors (wemos)
 def ts(str):
    s.send('w'.encode())
    data = ''
@@ -17,11 +24,21 @@ def tss(str):
    data1 = s.recv(2048).decode()
    print("Flame Sensor Value : ","<",data1,">")
    #1 its okey there is not flame in environment
-
-#data = ''
-
+#data1 = ''
+#functions for measuring the sensors (node)
+def tsss(str):
+   s2.send('a'.encode())
+   data2 = ''
+   data2 = s2.recv(2048).decode()
+   print("CO Sensor Value : ","<",data2,">")
+#data2=''
+#our main loop
 while 2:
-   r = input('enter')
+   r = input('Enter to measure values:')
    ts(s)
-
+   tss(s)
+   tsss(s)
+#closing sockets
 s.close ()
+s2.close()
+
